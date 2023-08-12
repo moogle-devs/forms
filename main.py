801bf396e2528f08d594791e02b10f06e6ec30ff
pɹolʇɐƆ <134858054+element10101@users.noreply.github.com>
2023-08-12 09:34:52 -0500

    `/save` endpoint for cloud

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from replit import web
+from requests import post
 
 app = Flask(__name__)
 
@@ -25,13 +27,20 @@ def index():
 
 @app.route("/edit")
 def edit():
-  return render_template("edit.html")
+  return render_template("edit.html", login=web.auth.name)
 
 
 @app.route("/form")
 def form():
   return render_template("form.html")
 
+@app.route('/save', methods=["POST"])
+def save2Cloud():
+  data = request.get_json(force=True)
+  cloud = "https://9d5ff3d2-795a-4609-bb47-a5d4d13bd681-forms.m00gle.repl.co/";
+  r = post(cloud, json=data)
+  r.raise_for_status()
+  return r.json()
 
 if __name__ == "__main__":
   app.run(host="0.0.0.0", port=81)
